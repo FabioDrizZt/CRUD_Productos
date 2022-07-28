7d3994f61b082df1ca0cd5e62f19c042e5a87f2a
Fabio DrizZt <ing.fabioarg@gmail.com>
2022-07-28 18:35:28 -0300

    Agregue la conexión a la base de datos

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php
+
+$PDO = new PDO("mysql:host=localhost; port=3306; dbname=crud_productos", "root", "");
+$PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
+
 <!doctype html>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
