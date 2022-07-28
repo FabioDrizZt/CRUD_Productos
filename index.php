b61ffd9dffa969a393cc0c14a704e2fe536748de
Fabio DrizZt <ing.fabioarg@gmail.com>
2022-07-28 18:50:50 -0300

    Cargue los productos en la tabla

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 $PDO = new PDO("mysql:host=localhost; port=3306; dbname=crud_productos", "root", "");
 $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$consulta = $PDO->prepare("SELECT * FROM productos ORDER BY fecha_creacion");
+$consulta->execute();
+$productos = $consulta->fetchAll(pdo::FETCH_ASSOC);
+/* echo "<pre>";
+var_dump($productos);
+echo "</pre>"; */
 ?>
 
 <!doctype html>
@@ -22,30 +28,25 @@ $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Imagen</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Descripcion</th>
+      <th scope="col">Precio</th>
+      <th scope="col">fecha de creacion</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+    <?php foreach ($productos as $i => $producto) {?>
+        <tr>
+        <th scope="row"><?=$i + 1?></th>
+        <td></td>
+        <td><?=$producto['nombre']?></td>
+        <td><?=$producto['descripcion']?></td>
+        <td><?=$producto['precio']?></td>
+        <td><?=$producto['fecha_creacion']?></td>
+        </tr>
+    <?php }?>
+    </tbody>
 </table>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
